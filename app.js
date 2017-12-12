a4f99cc3ca17c2335af758af1651d81bdbbacb3c
fangbao <fangbao@i-counting.cn>
2017-12-13 00:47:26 +0800

    no message

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // require('babel-register');
 const Koa = require('koa');
 const app = new Koa();
-
+const router = require('koa-router')();
+const koaBody = require('koa-body');
+const auth = require('koa-basic-auth');
 // x-response-time
+
 
 app.use(async (ctx, next) => {
   const start = Date.now();
@@ -10,7 +13,7 @@ app.use(async (ctx, next) => {
   const ms = Date.now() - start;
   ctx.set('X-Response-Time', `${ms}ms`);
 });
-
+app.use(koaBody());
 // logger
 
 app.use(async (ctx, next) => {
@@ -19,12 +22,36 @@ app.use(async (ctx, next) => {
   const ms = Date.now() - start;
   console.log(`${ctx.method} ${ctx.url} - ${ms}`);
 });
+router.get('/detail/:id', detail)
+
+app.use(router.routes());
+
+async function detail(ctx) {
+  console.log(ctx.params.id)
+  ctx.body = 'detail' + ctx.params.id;
+}
 
 // response
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
 
+app.use(async function(ctx, next) {
+  console.log(next)
+  try {
+    await next();
+  } catch (err) {
+    console.log(err)
+    if (err.status === 401) {
+      ctx.status = 401;
+      ctx.set('WWW-Authenticate', 'Basic');
+      ctx.body = 'cant haz that';
+    } else {
+      throw err;
+    }
+  }
+});
+app.use(auth({ name: 'tj', pass: '123' }));
+app.use(async function(ctx) {
+  ctx.body = 'secret';
+});
 
 app.listen(3000);
